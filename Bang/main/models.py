9dd09e4d37d6a525c50a0893b434a9ce6e5ad7b2
Emelian Nichiforv <pau4ok121@gmail.com>
2021-04-12 16:01:13 +0300

    Created 4 tables

--- Bang/main/models.py
+++ Bang/main/models.py
@@ -1,3 +1,49 @@
 from django.db import models
 
-# Create your models here.
+
+class School(models.Model):
+    name = "Школа"
+    text = models.TextField('Описание')
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        verbose_name = "Школа"
+        verbose_name_plural = "Школа"
+
+
+class Party(models.Model):
+    name = "Вечерухи"
+    text = models.TextField('Описание')
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        verbose_name = "Вечерухи"
+        verbose_name_plural = "Вечерухи"
+
+
+class Bar(models.Model):
+    name = "Бар"
+    text = models.TextField('Описание')
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        verbose_name = "Бар"
+        verbose_name_plural = "Бар"
+
+
+class Menu(models.Model):
+    name = "Меню"
+    text = models.TextField('Описание')
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        verbose_name = "Меню"
+        verbose_name_plural = "Меню"
