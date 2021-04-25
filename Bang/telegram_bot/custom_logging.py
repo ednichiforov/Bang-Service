62ace976dc3054d1ebb75ba45da7fa1fe40bbbf5
Emelian Nichiforv <pau4ok121@gmail.com>
2021-04-25 05:23:46 +0300

    Changed normal func to decorator

--- Bang/telegram_bot/custom_logging.py
+++ Bang/telegram_bot/custom_logging.py
@@ -7,10 +7,16 @@ logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s
 logger = logging.getLogger(__name__)
 
 
-def custom_info_logging(action, update):
-    user = update.message.from_user
-    logger.info(f"{user.first_name}:{user.username}:{action}")
+def custom_info_logging(func):
+    def inner(update, _):
+        user = update.message.from_user
+        logger.info(f"{user.first_name}/{user.username} pressed {func.__name__}")
+        return func(update, _)
+    return inner
 
 
-def custom_warning_logging(update, _):
-    logger.warning(f'Update {update} caused error {_}')
+def custom_warning_logging(func):
+    def inner(update, _):
+        logger.warning(f'Update {update} caused error {_}')
+        return func(update, _)
+    return inner
