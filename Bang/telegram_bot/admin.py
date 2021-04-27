8e064c0d12514c57df0a00075ff96ad1b944e2c2
Emelian Nichiforv <pau4ok121@gmail.com>
2021-04-27 04:26:40 +0300

    Blake changed the way it looks

--- Bang/telegram_bot/admin.py
+++ Bang/telegram_bot/admin.py
@@ -4,15 +4,36 @@ from .models import UsersStartedConv, PartyUsersForNearestParty, AllPartyUsers
 
 @admin.register(UsersStartedConv)
 class MainInfo(admin.ModelAdmin):
-    list_display = ["first_name", "last_name", "username", "user_id", "is_bot", "created_at"]
-    search_fields = ('username', 'user_id')
+    list_display = [
+        "first_name",
+        "last_name",
+        "username",
+        "user_id",
+        "is_bot",
+        "created_at",
+    ]
+    search_fields = ("username", "user_id")
 
 
 @admin.register(PartyUsersForNearestParty)
-class MainInfo(admin.ModelAdmin):
-    list_display = ["user", "real_name", "real_last_name", "number", "user_id"]
+class PartyInfo(admin.ModelAdmin):
+    list_display = [
+        "user",
+        "real_name",
+        "real_last_name",
+        "number",
+        "user_id",
+        "created_at",
+    ]
 
 
 @admin.register(AllPartyUsers)
-class MainInfo(admin.ModelAdmin):
-    list_display = ["user", "real_name", "real_last_name", "number", "user_id"]
+class AllPartiesInfo(admin.ModelAdmin):
+    list_display = [
+        "user",
+        "real_name",
+        "real_last_name",
+        "number",
+        "user_id",
+        "created_at",
+    ]
