fd034021674a20918704582b9a157f983dd01dcf
Emelian Nichiforv <pau4ok121@gmail.com>
2021-04-27 04:30:54 +0300

    Added unknown command

--- Bang/telegram_bot/bot_info_commands.py
+++ Bang/telegram_bot/bot_info_commands.py
@@ -3,32 +3,48 @@ from telegram import ReplyKeyboardMarkup, Update
 from telegram.ext import CallbackContext
 
 from main.models import Bar, Party, School
-
 from .db_usage import get_user_data_from_update
 from .custom_logging import custom_info_logging, custom_warning_logging
 
 
-buttons = [["DJ SCHOOL", "Вечеринки"], ["Барное меню", "Попасть в списки"]]
+BUTTONS = [["DJ SCHOOL", "Вечеринки"], ["Барное меню", "Попасть в списки"]]
 
-reply_markup = ReplyKeyboardMarkup(
-    keyboard=buttons, one_time_keyboard=True, resize_keyboard=True
+REPLY_MARKUP = ReplyKeyboardMarkup(
+    keyboard=BUTTONS, one_time_keyboard=True, resize_keyboard=True
 )
+
+ADMINS = [628061591, 247922134]
 
 
 @custom_info_logging
 def start(update: Update, _: CallbackContext) -> None:
     """Sends a message when a user starts a bot."""
+
     get_user_data_from_update(update)
 
     update.message.reply_text(
         text="Привет! Нажми на кнопку.",
-        reply_markup=reply_markup,
+        reply_markup=REPLY_MARKUP,
+    )
+
+
+@custom_info_logging
+def unknown_command(update: Update, _: CallbackContext) -> None:
+    """Sends a message when a user starts a bot."""
+
+    get_user_data_from_update(update)
+
+    update.message.reply_text(
+        text="Не понял тебя, нажми на кнопку ниже",
+        reply_markup=REPLY_MARKUP,
     )
 
 
 def commands(update: Update, _: CallbackContext):
     """ Analyzes users text massage """
+
     text = update.message.text
+
     if text == "DJ SCHOOL":
         return school(update=update, _=CallbackContext)
     elif text == "Барное меню":
@@ -39,16 +55,21 @@ def commands(update: Update, _: CallbackContext):
 
 def db_commands(Table) -> str:
     """ Takes text information from DB Table """
+
     text = str(Table.objects.only("text").last())
+
     return text
 
 
 @custom_info_logging
 def school(update: Update, _: CallbackContext) -> None:
     """ Sends text information about school """
+
     text = db_commands(School)
 
-    update.message.reply_text(text=text, reply_markup=reply_markup)
+    print(update)
+
+    update.message.reply_text(text=text, reply_markup=REPLY_MARKUP)
 
 
 @custom_info_logging
@@ -57,7 +78,7 @@ def bar(update: Update, _: CallbackContext) -> None:
 
     text = db_commands(Bar)
 
-    update.message.reply_text(text=text, reply_markup=reply_markup)
+    update.message.reply_text(text=text, reply_markup=REPLY_MARKUP)
 
 
 @custom_info_logging
@@ -72,7 +93,7 @@ def party(update: Update, _: CallbackContext) -> None:
     update.message.bot.send_photo(
         chat_id=update.effective_chat.id, photo=open(image, "rb")
     )
-    update.message.reply_text(text=text, reply_markup=reply_markup)
+    update.message.reply_text(text=text, reply_markup=REPLY_MARKUP)
 
 
 @custom_warning_logging
