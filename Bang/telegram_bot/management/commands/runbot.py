f179818a4235c938c82ab182f46721a0937b3004
Emelian Nichiforv <pau4ok121@gmail.com>
2021-04-27 04:32:21 +0300

    Added conversation for admin

--- Bang/telegram_bot/management/commands/runbot.py
+++ Bang/telegram_bot/management/commands/runbot.py
@@ -8,16 +8,24 @@ from telegram.ext import (
     ConversationHandler,
 )
 
-from telegram_bot.bot_info_commands import start, commands, error
+from telegram_bot.bot_info_commands import start, commands, error, unknown_command
 from telegram_bot.bot_registration_commands import (
     first_name_reg,
     last_name_reg,
     number_reg,
-    end,
-    cancel,
+    registration_end,
+    user_cancel,
+)
+from telegram_bot.bot_admin_commands import (
+    admin_starts_conv,
+    admin_loading_info,
+    admin_cancel,
 )
 
-FIRST_NAME_REG, LAST_NAME_REG, NUMBER_REG, END = range(4)
+
+FIRST_NAME_REG, LAST_NAME_REG, NUMBER_REG, USER_REGISTER_END = range(4)
+
+ADMIN_CONV, LOADING_INFO = range(2)
 
 
 class Command(BaseCommand):
@@ -38,37 +46,58 @@ class Command(BaseCommand):
             )
         )
 
-        conv_handler = ConversationHandler(
+        user_conv_handler = ConversationHandler(
             entry_points=[
                 MessageHandler(
                     filters=Filters.regex("^(Попасть в списки)$"),
                     callback=first_name_reg,
                 ),
-                CommandHandler(command="cancel", callback=cancel),
+                CommandHandler(command="user_cancel", callback=user_cancel),
             ],
             states={
                 FIRST_NAME_REG: [
                     MessageHandler(filters=Filters.text, callback=first_name_reg),
-                    CommandHandler(command="cancel", callback=cancel),
+                    CommandHandler(command="user_cancel", callback=user_cancel),
                 ],
                 LAST_NAME_REG: [
                     MessageHandler(filters=Filters.text, callback=last_name_reg),
-                    CommandHandler(command="cancel", callback=cancel),
+                    CommandHandler(command="user_cancel", callback=user_cancel),
                 ],
                 NUMBER_REG: [
                     MessageHandler(filters=Filters.text, callback=number_reg),
-                    CommandHandler(command="cancel", callback=cancel),
+                    CommandHandler(command="user_cancel", callback=user_cancel),
                 ],
-                END: [MessageHandler(filters=Filters.text, callback=end)],
+                USER_REGISTER_END: [
+                    MessageHandler(filters=Filters.text, callback=registration_end),
+                ],
             },
-            fallbacks=[CommandHandler(command="cancel", callback=cancel)],
+            fallbacks=[CommandHandler(command="user_cancel", callback=user_cancel)],
             allow_reentry=True,
         )
 
-        dispatcher.add_handler(conv_handler)
+        dispatcher.add_handler(user_conv_handler)
 
-        dispatcher.add_error_handler(error)
+        admin_conv_handler = ConversationHandler(
+            entry_points=[
+                CommandHandler(command="Gappa", callback=admin_starts_conv),
+                CommandHandler(command="admin_cancel", callback=admin_cancel),
+            ],
+            states={
+                ADMIN_CONV: [
+                    MessageHandler(filters=Filters.all, callback=admin_starts_conv),
+                    CommandHandler(command="admin_cancel", callback=admin_cancel),
+                ],
+                LOADING_INFO: [
+                    MessageHandler(filters=Filters.all, callback=admin_loading_info),
+                ],
+            },
+            fallbacks=[CommandHandler(command="admin_cancel", callback=admin_cancel)],
+            allow_reentry=True,
+        )
 
+        dispatcher.add_handler(admin_conv_handler)
+        # dispatcher.add_error_handler(error)
+        dispatcher.add_handler(MessageHandler(Filters.all, callback=unknown_command))
         updater.start_polling()
 
         updater.idle()
