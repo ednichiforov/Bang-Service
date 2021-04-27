0217b688310108361a055f128680545fb3e01ccf
Emelian Nichiforv <pau4ok121@gmail.com>
2021-04-27 04:30:34 +0300

    Added verbose name to user

--- Bang/telegram_bot/models.py
+++ Bang/telegram_bot/models.py
@@ -4,13 +4,19 @@ from django.db import models
 class UsersStartedConv(models.Model):
     user_id = models.IntegerField(primary_key=True, verbose_name="Телеграм id")
     first_name = models.CharField(max_length=30, null=True, verbose_name="Имя в ТГ")
-    last_name = models.CharField(max_length=30, null=True, blank=True, verbose_name="Фамилия в ТГ")
-    username = models.CharField(max_length=30, null=True, blank=True, verbose_name="Никнеим в ТГ")
+    last_name = models.CharField(
+        max_length=30, null=True, blank=True, verbose_name="Фамилия в ТГ"
+    )
+    username = models.CharField(
+        max_length=30, null=True, blank=True, verbose_name="Никнеим в ТГ"
+    )
     is_bot = models.BooleanField(verbose_name="Бот")
-    created_at = models.DateTimeField(auto_now_add=True, verbose_name="Зарегистрирован в Боте")
+    created_at = models.DateTimeField(
+        auto_now_add=True, verbose_name="Зарегистрирован в Боте"
+    )
 
     def __str__(self):
-        return f'@{self.username}' if self.username is not None else f'{self.user_id}'
+        return f"@{self.username}" if self.username is not None else f"{self.user_id}"
 
     class Meta:
         db_table = "Telegram_All_Users"
@@ -19,11 +25,21 @@ class UsersStartedConv(models.Model):
 
 
 class PartyUsersForNearestParty(models.Model):
-    user = models.ForeignKey(UsersStartedConv, on_delete=models.CASCADE)
-    real_name = models.CharField(max_length=30, null=False, verbose_name="Имя для вечеринки")
-    real_last_name = models.CharField(max_length=30, null=False, verbose_name="Фамилия для вечеринки")
-    number = models.CharField(max_length=30, null=False, verbose_name="Номер для вечеринки")
-    created_at = models.DateTimeField(auto_now_add=True)
+    user = models.OneToOneField(
+        UsersStartedConv, on_delete=models.CASCADE, verbose_name="Пользователь"
+    )
+    real_name = models.CharField(
+        max_length=30, null=False, verbose_name="Имя для вечеринки"
+    )
+    real_last_name = models.CharField(
+        max_length=30, null=False, verbose_name="Фамилия для вечеринки"
+    )
+    number = models.CharField(
+        max_length=30, null=False, verbose_name="Номер для вечеринки"
+    )
+    created_at = models.DateTimeField(
+        auto_now_add=True, verbose_name="Зарегестрировался"
+    )
 
     def __str__(self):
         return f"Id: {self.user}, {self.real_name}, зарегистрировался {self.created_at.strftime('(%H:%M, %d %B %Y)')}"
@@ -35,10 +51,18 @@ class PartyUsersForNearestParty(models.Model):
 
 
 class AllPartyUsers(models.Model):
-    user = models.ForeignKey(UsersStartedConv, on_delete=models.CASCADE)
-    real_name = models.CharField(max_length=30, null=False, verbose_name="Имя для вечеринки")
-    real_last_name = models.CharField(max_length=30, null=False, verbose_name="Фамилия для вечеринки")
-    number = models.CharField(max_length=30, null=False, verbose_name="Номер для вечеринки")
+    user = models.ForeignKey(
+        UsersStartedConv, on_delete=models.CASCADE, verbose_name="Пользователь"
+    )
+    real_name = models.CharField(
+        max_length=30, null=False, verbose_name="Имя для вечеринки"
+    )
+    real_last_name = models.CharField(
+        max_length=30, null=False, verbose_name="Фамилия для вечеринки"
+    )
+    number = models.CharField(
+        max_length=30, null=False, verbose_name="Номер для вечеринки"
+    )
     created_at = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
