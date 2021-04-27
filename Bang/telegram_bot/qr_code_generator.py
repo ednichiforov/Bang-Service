f62715f570026d091a78217e98c8d7d391f81a26
Emelian Nichiforv <pau4ok121@gmail.com>
2021-04-27 04:32:01 +0300

    Changed ORM query in user_id

--- Bang/telegram_bot/qr_code_generator.py
+++ Bang/telegram_bot/qr_code_generator.py
@@ -12,14 +12,14 @@ def qr_code_image(update):
     """ Makes QR code """
 
     user = update.message.from_user
-    user_id = str(UsersStartedConv.objects.get(user_id=user.id))
+    user_id = UsersStartedConv.objects.values("user_id").get(user_id=user.id)["user_id"]
     input_data = website + "/users/" + str(user_id)
 
     pillow_image = qrcode.make(input_data)
 
     byteImgIO = BytesIO()
     byteImgIO.name = str(user_id) + ".jpeg"
-    pillow_image.save(byteImgIO, 'JPEG')
+    pillow_image.save(byteImgIO, "JPEG")
     byteImgIO.seek(0)
 
     return byteImgIO
