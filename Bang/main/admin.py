9838ca2338686b80b1d3e12982bbd9b892162323
Emelian Nichiforv <pau4ok121@gmail.com>
2021-04-29 01:20:23 +0300

    Added info table

--- Bang/main/admin.py
+++ Bang/main/admin.py
@@ -1,7 +1,12 @@
 from django.contrib import admin
-from .models import School, Bar, Party
+from .models import School, Bar, Party, About
 
 
-@admin.register(School, Bar, Party)
+@admin.register(School, Bar, About)
 class MainInfo(admin.ModelAdmin):
     list_display = ["text"]
+
+
+@admin.register(Party)
+class AboutInfo(admin.ModelAdmin):
+    list_display = ["text", "picture"]
