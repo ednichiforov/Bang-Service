922ccfa3268410c66f68ffbac286cee48bfc663b
Emelian Nichiforv <pau4ok121@gmail.com>
2021-04-29 01:19:49 +0300

    Created a website

--- Bang/main/urls.py
+++ Bang/main/urls.py
@@ -2,9 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.general, name="general"),
-    path('school', views.school, name="school"),
-    path('party', views.party, name="party"),
-    path('bar', views.bar, name="bar"),
-    path('users/<str:user_id>', views.users, name="users"),
+    path("", views.general, name="general"),
+    path("users/<str:user_id>", views.users, name="users"),
 ]
--- Bang/main/views.py
+++ Bang/main/views.py
@@ -1,27 +1,25 @@
 from django.shortcuts import render
-from .models import School, Party, Bar
+from .models import School, Party, Bar, About
 from telegram_bot.models import PartyUsersForNearestParty
 
 
 def general(request):
-    return render(request, "main/base.html")
-
-
-def school(request):
-    text = School.objects.only("text").last()
-    return render(request, "main/school.html", {"text": text})
-
-
-def party(request):
-    text = Party.objects.only("text").last()
-    return render(request, "main/party.html", {"text": text})
-
-
-def bar(request):
-    text = Bar.objects.only("text").last()
-    return render(request, "main/bar.html", {"text": text})
+    bar_text = Bar.objects.only("text").last()
+    school_text = School.objects.only("text").last()
+    party_text = Party.objects.only("text").last()
+    about_text = About.objects.only("text").last()
+    picture_name = getattr(Party.objects.last(), "picture")
+    text_field = {
+        "bar_text": bar_text,
+        "school_text": school_text,
+        "party_text": party_text,
+        "about_text": about_text,
+        "picture_name": picture_name,
+    }
+    return render(request, "main/index.html", text_field)
 
 
 def users(request, user_id):
     PartyUsersForNearestParty.objects.get(user_id=user_id)
-    return render(request, "main/users.html")
+    picture_name = getattr(Party.objects.last(), "picture")
+    return render(request, "main/users.html", {"picture_name": picture_name})
