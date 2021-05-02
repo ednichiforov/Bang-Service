4e76764a6326262581525e45bdd57f2a702106e0
Emelian Nichiforv <pau4ok121@gmail.com>
2021-05-02 21:49:12 +0300

    Added try/except for picture field

--- Bang/main/views.py
+++ Bang/main/views.py
@@ -3,27 +3,31 @@ from .models import School, Party, Bar, About
 from telegram_bot.models import PartyUsersForNearestParty
 from django.conf import settings
 
+try:
+    picture_name = getattr(Party.objects.last(), "picture")
+except AttributeError:
+    picture_name = None
+
+party_picture = f"{settings.MEDIA_URL}{picture_name}"
+
 
 def general(request):
     bar_text = Bar.objects.only("text").last()
     school_text = School.objects.only("text").last()
     party_text = Party.objects.only("text").last()
     about_text = About.objects.only("text").last()
-    picture_name = getattr(Party.objects.last(), "picture")
-    media = f"{settings.MEDIA_URL}{picture_name}"
-    text_field = {
+
+    db_info = {
         "bar_text": bar_text,
         "school_text": school_text,
         "party_text": party_text,
         "about_text": about_text,
-        "picture_name": picture_name,
-        "media": media,
+        "party_picture": party_picture,
     }
-    return render(request, "main/index.html", text_field)
+
+    return render(request, "main/index.html", db_info)
 
 
 def users(request, user_id):
     PartyUsersForNearestParty.objects.get(user_id=user_id)
-    picture_name = getattr(Party.objects.last(), "picture")
-    media = f"{settings.MEDIA_URL}{picture_name}"
-    return render(request, "main/users.html", {"media": media})
+    return render(request, "main/users.html", {"party_picture": party_picture})
