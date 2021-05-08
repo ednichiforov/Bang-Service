514fc1fc503093a82aca74f2a7a098c4fc229065
Emelian Nichiforv <pau4ok121@gmail.com>
2021-05-08 04:18:51 +0300

    Changed dockerfile for production

--- Bang/entrypoint.sh
+++ Bang/entrypoint.sh
@@ -1,8 +1,6 @@
 #!/bin/bash
 
-python3 manage.py makemigrations --no-input
 python3 manage.py migrate --no-input
-python3 manage.py createcachetable
-python3 manage.py runbot
 
-exec gunicorn Bang.wsgi:application -b 0.0.0.0:8000 --reload
+
+python3 manage.py runbot | gunicorn Bang.wsgi:application --bind 0.0.0.0:8080
